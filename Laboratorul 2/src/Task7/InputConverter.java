af6feccf29c056bc987fd67a49f177428d26af83
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2024-03-12 21:41:15 +0200

    Second Commit Lab2

--- Laboratorul 2/src/Task7/InputConverter.java
+++ Laboratorul 2/src/Task7/InputConverter.java
@@ -3,15 +3,34 @@ package Task7;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputConverter {
     public static List<CalculatorRequest> mapRequests(String[] args) {
         List<CalculatorRequest> requests = new ArrayList<>();
         for (int i = 0; i < args.length; i += 3) {
             if (i + 2 < args.length) {
                 try {
-                    Object leftOperand = Double.parseDouble(args[i]);
-                    Object rightOperand = Double.parseDouble(args[i + 2]);
-                    requests.add(new CalculatorRequest(leftOperand, rightOperand, args[i + 1]));
+                    Object leftOperand;
+                    Object rightOperand;
+                    String operation = args[i + 1];
+
+                    if (isInteger(args[i]) && isInteger(args[i + 2])) {
+                        leftOperand = Integer.parseInt(args[i]);
+                        rightOperand = Integer.parseInt(args[i + 2]);
+                    } else if (isDouble(args[i]) && isDouble(args[i + 2])) {
+                        leftOperand = Double.parseDouble(args[i]);
+                        rightOperand = Double.parseDouble(args[i + 2]);
+                    } else if (isBoolean(args[i]) && isBoolean(args[i + 2])) {
+                        leftOperand = Boolean.parseBoolean(args[i]);
+                        rightOperand = Boolean.parseBoolean(args[i + 2]);
+                    } else {
+                        // Ignorăm operațiile invalide
+                        continue;
+                    }
+
+                    requests.add(new CalculatorRequest(leftOperand, rightOperand, operation));
                 } catch (NumberFormatException e) {
                     // Ignorăm operațiile invalide
                 }
@@ -19,4 +38,27 @@ public class InputConverter {
         }
         return requests;
     }
+
+    private static boolean isInteger(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isDouble(String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isBoolean(String value) {
+        return value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false");
+    }
 }
+
